a60dc3bf797af1840509280b0945ca7aac2ef41b
Kevin Patrick Murphy <kpm2@csg21-27.ucc.ie>
2014-11-12 14:34:07 +0000

    Working on second function of Parallel Assignment

--- CS4402/assignment/SortingAssignment.c
+++ CS4402/assignment/SortingAssignment.c
@@ -6,7 +6,7 @@
 void compare_and_exchange(int n, double * a, int rank, MPI_Status status);
 
 //Function Declerations
-int MPI_Sort(int n, double * array, int root, MPI_Comm comm);
+int MPI_Odd_even_sort(int n, double * array, int root, MPI_Comm comm);
 int MPI_Exhange(int n, double * array, int rank1, int rank2, MPI_Comm comm);
 int MPI_Is_sorted(int n, double * array, int rank1, int rank2, MPI_Comm comm);
 
@@ -30,12 +30,14 @@ int main (int argc, char *argv[])
     MPI_Init(&argc, &argv);
        
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    a = genRandomArray(n);
 
-    MPI_Exchange(n, a, 0, 1, MPI_COMM_WORLD);
-
+    if(rank == 0)
+    {
+	printf("Size of Comm World : %d\n", size);
+        a = genRandomArray(n);
+    }	
+    MPI_Odd_even_sort(n, a, 0, MPI_COMM_WORLD);
     MPI_Finalize();
 }
 
@@ -63,28 +65,19 @@ int MPI_Exchange(int n, double * array, int rank1, int rank2, MPI_Comm comm)
 {
     int tag1 = 0, tag2 = 1, i;
 
-
-
     b = (double *)calloc(n, sizeof(double));
-    //c = (double *)calloc(n * 2, sizeof(double));
-
-    printf("Calling MPI_Exchange...From Rank %d\n\n", rank);
 
     if(rank==rank1)
     {
 	//Send Unsorted Array
-	MPI_Send(array, n, MPI_DOUBLE,i rank2, tag1, comm);
+	MPI_Send(array, n, MPI_DOUBLE, rank2, tag1, comm);
 
 	//Recieve
 	MPI_Recv(b, n, MPI_DOUBLE, rank2, tag2, comm, &status);
-	
-	printf("Rank %d Recieved Array B: \n ", rank);
-	print_array(b, n);
 
 	//Merge
 	c = merge_array(n, array, n, b);
- 	
-	printf("Rank %d Merged Array\n", rank);
+
 	for(i=0;i<n;i++){
 	     array[i] = c[i];
 	}
@@ -96,84 +89,87 @@ int MPI_Exchange(int n, double * array, int rank1, int rank2, MPI_Comm comm)
 	//Recieve
 	MPI_Recv(b, n, MPI_DOUBLE, rank1, tag1, comm, &status);
         
-	//printf("Rank %d Recieved Array B: \n", rank);
-	//print_array(b, n);
-
 	//Send Unsorted Array
 	MPI_Send(array, n, MPI_DOUBLE, rank1, tag2, comm);
 
 	//Merge
 	c = merge_array(n, array, n, b);
 
-	//printf("Rank %d Merged Array\n", rank);
-
 	for(i=0;i<n;i++){
 	     array[i] = c[i+n];
         }
-	//print_array(array, n);
+	print_array(array, n);
     }
 
     return MPI_SUCCESS; //MPI_SUCCESS;
 }
 
-int MPI_Sort(int n, double * array, int root, MPI_Comm comm);
+int MPI_Odd_even_sort(int n, double * array, int root, MPI_Comm comm)
 {
 	double * localArray = (double *)calloc(n/size, sizeof(double));
-	//Scatter
-        MPI_Scatter(array, n/size, MPI_DOUBLE, localArray, n/size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-        // sort scattered_array
-        merger_sort(n/size, localArray)
+	if(rank == root)
+	{
+	    //Scatter
+            MPI_Scatter(&array[0], n/size, MPI_DOUBLE, localArray, n/size, MPI_DOUBLE, 0, comm);
+	}
+
+        //Sort localArray
+        merge_sort(n/size, localArray);
+
+	printf("Merged Local Array\n");
+	print_array(localArray, n/size);
 
 	//Repeart for step=0,1,2..., size-1
 	for(i=0; i < size; i++)
 	{
-	    if((i+rank) % 2 == 0)
+	    printf("Loop: %d, Rank: %d \n", i, rank);
+
+	    if(((rank) % 2) == 0)
 	    {
-		if(rank < size-1)
+		printf("Rank %d, Even\n", rank);
+		if(rank < size - 1)
 		{
-		    MPI_Exchange(n/size, localArray, rank, rank+1, MPI_COMM_WORLD);
-		}
-		else
-		{
-		    if(rank > 0)
-		    {
-		        MPI_Exchange(n/size, localArray, rank-1, rank, MPI_COMM_WORLD);
-		    }
+		    MPI_Exchange(n/size, localArray, rank, rank+1, comm);
 		}
 	    }
-	    MPI_Barrier(MPI_COMM_WORLD);
+	    else
+	    {
+		    printf("Rank %d, Odd\n", rank);
+		    if(rank > 0)
+		    {
+		        MPI_Exchange(n/size, localArray, rank-1, rank, comm);
+		    }
+	    }
+	
+	    //MPI_Barrier(MPI_COMM_WORLD);
 	}
 
-        //Gather
-        MPI_Gather(localArray, n/size, MPI_Double, array, n/size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-	if(rank == 0)
+	if(rank == root)
 	{
+	    //Gather			
+            MPI_Gather(&localArray[0], n/size, MPI_DOUBLE, &array[0], n/size, MPI_DOUBLE, 0, comm);
 	    for(x = 0; x < n; x++)
 	    {
 		printf("Output : %f\n", array[x]);
 	    }
 	}
-}
 
+	return MPI_SUCCESS;
+}
 
 double * merge_array(int n, double * a, int m, double * b){
    int i,j,k;
    double * f = (double *) calloc(n+m, sizeof(double));
 
-   for(i=j=k=0;(i<n)&&(j<m);){
+   for(i=j=k=0;(i<n)&&(j<m);)
       if(a[i]<=b[j])f[k++]=a[i++];
       else f[k++]=b[j++];
       if(i==n)for(;j<m;)f[k++]=b[j++];
       else for(;i<n;)f[k++]=a[i++];
-   }
-   printf("MERGE_ARRAY\n");
-   print_array(f, n + m);
+   
    return f;
 }
-
-
 
 void merge_sort(int n, double * a){
 
