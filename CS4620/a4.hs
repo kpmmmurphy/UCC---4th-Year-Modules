c9ed7ce127bb3afa2f41aa6c0f5835b6271e65f7
Kevin Patrick Murphy <kpm2@mmlab02.ucc.ie>
2014-11-14 17:27:51 +0000

    Starting SquareRoot

--- CS4620/a4.hs
+++ CS4620/a4.hs
@@ -4,8 +4,10 @@ computeRoot a x = (a + (x/a)) / 2
 
 -- approximate x xs : 
 --approx' :: Float -> Float -> [zs]
---approx' _ [_] = 1.0
-approx' x y = computeRoot y x : approx' x (computeRoot y x) 
+approx' x y =  computeRoot y x : approx' x (computeRoot y x) 
 
 --approx :: Float -> [xs] -> [ys]
-approx x = approx' x 1.0
+approx x = 1.0 : approx' x 1.0
+
+--squareRoot :: 
+squareRoot' x = [let n3 = n1 - n2 | n1 <- approx x, n2 <- tail (approx x), n3 <= 0.0005]
