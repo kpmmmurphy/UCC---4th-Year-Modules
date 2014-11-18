87c8875895dfbe2bbe1af5fbf28711a346b47712
kpmmmurphy <kpmmmurphy@gmail.com>
2014-11-18 22:00:38 +0000

    Still working on squareRoot

--- CS4620/a4.hs
+++ CS4620/a4.hs
@@ -1,20 +1,28 @@
--- computeRoot a x:
+-- computeRoot a x: Formula to compute the root of 'x' using previously 
+--                  computed approximate square root value 'a' 
 computeRoot :: Float -> Float -> Float
 computeRoot a x = (a + (x/a)) / 2
+ 
+--approx' x y :: Helper function to compute the infinite list of square root
+--               approximations of a given value 'x', using 'y' to increase
+--               precision of approximation
+approx' :: Float -> Float -> [Float]
+approx' x y = computeRoot y x : approx' x (computeRoot y x) 
 
--- approximate x xs : 
---approx' :: Float -> Float -> [zs]
-approx' x y =  computeRoot y x : approx' x (computeRoot y x) 
+--approx x :: An infinite list of approximations which coverage to the square 
+--            root of 'x'
+approx :: Float -> [Float]
+approx x = approx' x 1.0
 
---approx :: Float -> [xs] -> [ys]
-approx x = 1.0 : approx' x 1.0
+--squareRoot' :: Helper function to estimate the square root of 'x'
+squareRoot' :: [Float] -> Float
+squareRoot' (n1:n2:ns) | abs ((-) n1 n2) <= 0.0001 = n2
+                       | otherwise = squareRoot' ns
 
---squareRoot :: 
-squareRoot' ( n1 :n2 : ns ) | abs ((-) n1 n2) > 0.0001 = squareRoot' ns
-                            | otherwise = n2
-
+--squareRoot :: Gives an extimation of the square root of 'x'
+squareRoot :: Float -> Float
 squareRoot x = squareRoot' (approx x)
 
-toRoot  x = [ n | n <- [0 .. squareRoot x]]
-toRoot' x = [ n1 | n1 <- [0 .. squareRoot x], n2 <- [1 .. squareRoot x], ((/) n2 n1) /= 0]
---primes = [n \]
+toRoot = [ n2 | n1 <- [ 1 .. ], n2 <- [ [ n3 | n3 <- [ 3 .. squareRoot n1 ], ((/) n1 n3 /= 0) ]]]
+
+
