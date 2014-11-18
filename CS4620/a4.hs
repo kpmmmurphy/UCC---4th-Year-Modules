64e802eff7f48f42d873d162b9fb3db2bff0d999
Kevin Patrick Murphy <kpm2@cs1-32-10.ucc.ie>
2014-11-18 15:18:18 +0000

    Working on Primes

--- CS4620/a4.hs
+++ CS4620/a4.hs
@@ -10,4 +10,11 @@ approx' x y =  computeRoot y x : approx' x (computeRoot y x)
 approx x = 1.0 : approx' x 1.0
 
 --squareRoot :: 
-squareRoot' x = [let n3 = n1 - n2 | n1 <- approx x, n2 <- tail (approx x), n3 <= 0.0005]
+squareRoot' ( n1 :n2 : ns ) | abs ((-) n1 n2) > 0.0001 = squareRoot' ns
+                            | otherwise = n2
+
+squareRoot x = squareRoot' (approx x)
+
+toRoot  x = [ n | n <- [0 .. squareRoot x]]
+toRoot' x = [ n1 | n1 <- [0 .. squareRoot x], n2 <- [1 .. squareRoot x], ((/) n2 n1) /= 0]
+--primes = [n \]
